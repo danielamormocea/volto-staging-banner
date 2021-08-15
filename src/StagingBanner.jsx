25a7f5c6877234e267e0d67592d1f000ea883c2e
Daniela Mormocea <daniela.mormocea@gmail.com>
2021-08-15 14:58:40 +0300

    add demo banner functionality

---
+++ src/StagingBanner.jsx
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import './stagingBanner.css';
+
+const StagingBanner = () => {
+
+  let [path, setPath]= React.useState();
+  let [offset, setOffset] = React.useState(0);
+  
+  React.useEffect(function mount() {
+    setPath(window.location.host);
+    window.onscroll = () => {
+      setOffset(window.pageYOffset);
+
+    }
+  }, []);
+
+  const demoIdentifiers = ['dev', 'demo', 'staging', 'localhost'];
+
+  if (path !== undefined && (demoIdentifiers.reduce((acc, identifier) => acc || path.includes(identifier), false))) {
+    return (
+      <div>
+      <div className="stagingBanner" style={{ position: offset > 0 ? "fixed" : "relative" }}>
+        <p> <i class="exclamation circle icon"></i> This is a demo/test instance. Do not use it for operational purposes. All changes will be regularly overwritten. </p>
+      </div>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default StagingBanner;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import { StagingBanner } from './StagingBanner';
+
 const applyConfig = (config) => {
+  const widgets = config.widgets;
+  widgets = {
+    ...widgets,
+    id: {
+      ...widgets.id,
+      stagingBanner: StagingBanner
+    }
+  }
   return config;
 };
 
